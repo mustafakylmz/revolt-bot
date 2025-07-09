f05c3c707a106a30546c8428c0d6bf0c0883c361
mustafakylmz <76853281+mustafakylmz@users.noreply.github.com>
2025-07-09 23:58:31 +0300

    v1.1

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
+const express = require('express');
+const { InteractionType, verifyKeyMiddleware } = require('discord-interactions');
+const { InteractionResponseType } = require('discord-api-types/v10');
 require('dotenv').config();
-const { Client, GatewayIntentBits, Events } = require('discord.js');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-client.once(Events.ClientReady, () => {
-  console.log(`Bot hazır! ${client.user.tag}`);
+// Discord tarafından gönderilen istekleri doğrulamak için middleware
+app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), (req, res) => {
+  const interaction = req.body;
+
+  if (interaction.type === InteractionType.PING) {
+    return res.send({ type: InteractionResponseType.PONG });
+  }
+
+  // Slash komutun geldiği yer
+  if (interaction.type === InteractionType.APPLICATION_COMMAND) {
+    return res.send({
+      type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+      data: {
+        content: 'Faceit sistemine hoş geldin!',
+      },
+    });
+  }
 });
 
-client.login(process.env.BOT_TOKEN);
+app.listen(PORT, () => {
+  console.log(`Bot sunucusu çalışıyor: http://localhost:${PORT}`);
+});
