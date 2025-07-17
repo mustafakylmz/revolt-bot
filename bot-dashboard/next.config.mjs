ac4eb682251dc6836f15280ebf99ebe89688861e
mustafakylmz <76853281+mustafakylmz@users.noreply.github.com>
2025-07-17 14:58:46 +0300

    next.config.mjs değişimi

---
+++ bot-dashboard/next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // React Strict Mode'u etkinleştirir. Geliştirme sırasında potansiyel sorunları belirlemeye yardımcı olur.
+  reactStrictMode: true,
+};
+
+export default nextConfig;
